718451fb4fac205df016afe321f774e15d5bbb12
q4speed <kun@fit2cloud.com>
2021-03-19 16:31:41 +0800

    refactor(合规报告): 优化页面

---
+++ frontend/src/components/LabelValue.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="label-value">
+    <label>{{ label }}</label>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LabelValue",
+  props: {
+    label: String,
+    value: String,
+  }
+}
+</script>
+
+<style scoped>
+.label-value {
+  display: flex;
+  justify-content: flex-start;
+}
+
+.label-value label {
+  margin: 0 20px;
+  text-align: left;
+  color: #909090;
+  width: 120px;
+  flex-grow: 0;
+  flex-shrink: 0;
+}
+</style>
