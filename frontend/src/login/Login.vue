8139f3a232af2052e519493953205498384ab584
zhangdahai112 <dahai.zhang@fit2cloud.com>
2021-03-19 17:47:46 +0800

    refactor:去除多余代码

--- frontend/src/login/Login.vue
+++ frontend/src/login/Login.vue
@@ -14,7 +14,6 @@
           <div class="form">
             <el-form-item v-slot:default>
               <el-radio-group v-model="form.authenticate">
-<!--                <el-radio label="LDAP" size="mini" v-if="openLdap">LDAP</el-radio>-->
                 <el-radio label="LOCAL" size="mini" v-if="openLdap">普通登录</el-radio>
               </el-radio-group>
             </el-form-item>
