ae33cb0ecd9cea5c148939ce4c2e158b5781ecea
maguohao2018 <guohao.ma@fit2cloud.com>
2021-05-18 14:41:41 +0800

    feat(sql): 新增 OpenStack 内置规则。

--- backend/src/main/resources/db/migration/V18__insert_openstack_plugin.sql
+++
@@ -1,7 +0,0 @@
-
-insert into plugin ( id, name, icon, update_time) values ('fit2cloud-openstack-plugin', 'OpenStack', 'openstack.png', concat(unix_timestamp(now()), '001'));
-insert into plugin ( id, name, icon, update_time) values ('fit2cloud-vsphere-plugin', 'VMware vSphere', 'vmware.png', concat(unix_timestamp(now()), '001'));
-
-INSERT INTO rule_group (`name`, `description`, `level`, `plugin_id`, `flag`) VALUES ('OpenStack 安全检查', '安全检查，为您提供通信网络、计算环境和管理中心的网络安全检查。', '等保三级', 'fit2cloud-openstack-plugin', 1);
-INSERT INTO rule_group (`name`, `description`, `level`, `plugin_id`, `flag`) VALUES ('VMware 安全检查', '安全检查，为您提供通信网络、计算环境和管理中心的网络安全检查。', '等保三级', 'fit2cloud-vsphere-plugin', 1);
-
